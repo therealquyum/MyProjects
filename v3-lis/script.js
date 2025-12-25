72161b44174e98b92f42e72f5b96ece7a99e0e45
Abdul Quyum <abdulquyum246@gmail.com>
2025-12-25 15:03:21 +0100

    add

--- v3-lis/script.js
+++ v3-lis/script.js
@@ -3,3 +3,19 @@ function showMessage() {
 }
 
 
+  const text = "Welcome to V3 LIS Resource Hub";
+  const speed = 100; // typing speed in milliseconds
+  let index = 0;
+
+  function typeEffect() {
+    if (index < text.length) {
+      document.getElementById("type-text").textContent += text.charAt(index);
+      index++;
+      setTimeout(typeEffect, speed);
+    }
+  }
+
+  // Start typing when page loads
+  window.onload = typeEffect;
+
+
