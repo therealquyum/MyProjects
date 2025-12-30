1cef7e24cb5fa6e26cd8f4e74a3a3d9d6893f85a
Abdul Quyum <abdulquyum246@gmail.com>
2025-12-30 15:32:38 +0100

    add

--- v3-lis/script.js
+++ v3-lis/script.js
@@ -2,20 +2,9 @@ function showMessage() {
   alert("Still on maintanace Sen. Adeyemi Quyum Adekunle.)
 }
 
-
-  const text = "Welcome to V3 LIS Resource Hub";
-  const speed = 100; // typing speed in milliseconds
-  let index = 0;
-
-  function typeEffect() {
-    if (index < text.length) {
-      document.getElementById("type-text").textContent += text.charAt(index);
-      index++;
-      setTimeout(typeEffect, speed);
-    }
-  }
-
-  // Start typing when page loads
-  window.onload = typeEffect;
-
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+    menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('show');
+    });
 
